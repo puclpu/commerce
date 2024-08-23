1282eb96b0a49a5a5aebc3b21c4304e28a5d60f8
kimchaerin <puclpu18@gmail.com>
2024-08-23 13:35:06 +0900

    ✨ [#5] product entity 생성

---
+++ commerce/src/main/java/com/sparta/commerce/product/entity/Product.java
@@ -0,0 +1,46 @@
+package com.sparta.commerce.product.entity;
+
+import com.sparta.commerce.global.entity.Timestamped;
+import com.sparta.commerce.product.type.Category;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "product")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Product extends Timestamped {
+
+  @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "product_id")
+  private Long id;
+
+  @Column(name = "name", length = 50, nullable = false)
+  private String name;
+
+  @Column(name = "price", nullable = false)
+  private int price;
+
+  @Column(name = "description", length = 500, nullable = false)
+  private String description;
+
+  @Column(name = "stock", nullable = false)
+  private int stock;
+
+  @Column(name = "thumbnail_image", length = 512, nullable = false)
+  private String thumbnailImage;
+
+  @Column(name = "category", nullable = false)
+  @Enumerated(value = EnumType.STRING)
+  private Category category;
+
+}
