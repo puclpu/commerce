122de93226263c7cc3897e538a3512d8f5f152c9
kimchaerin <puclpu18@gmail.com>
2024-08-25 16:46:00 +0900

    🔥 [#19] unused import statement 삭제

--- commerce/src/main/java/com/sparta/commerce/product/repository/ProductOptionRepository.java
+++ commerce/src/main/java/com/sparta/commerce/product/repository/ProductOptionRepository.java
@@ -1,7 +1,6 @@
 package com.sparta.commerce.product.repository;
 
 import com.sparta.commerce.product.entity.ProductOption;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductOptionRepository extends JpaRepository<ProductOption, Long>, ProductOptionQueryDSLRepository {
